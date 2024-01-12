0ee266731e194d3273115b4b036fc14314a17135
Koyo Fujii <126226234+Koyo8144@users.noreply.github.com>
2024-01-13 00:44:28 +0900

    Update unit1_exercise.cpp

--- c_scripts/src/unit1_exercise.cpp
+++ c_scripts/src/unit1_exercise.cpp
@@ -1,1 +1,57 @@
-edit test
+/*
+This C++ line includes a header file named rosbot_class.h 
+from the rosbot_control directory.
+By including the rosbot_class.h header file, 
+the C++ program can access the classes, functions, and variables 
+defined in that header file.
+*/
+#include "rosbot_control/rosbot_class.h"
+/*
+This line of C++ code includes the ROS header file ros.h 
+from the ros package, we import this library 
+to make use of the classes and functions that ROS provides.
+*/
+#include <ros/ros.h>
+/*
+Note that when including header files, we use " " (quotes) or < > (angle bracket) syntax. 
+Using <> tells the preprocessor to search the usual location for system headers 
+in order to find the required header, whereas using "" tells the preprocessor 
+to first search the directory where the current file is located 
+before searching the usual path for system headers.
+*/
+/*
+This line means that we can use the code in the std namespace 
+without typing std:: before it. For example, 
+we can write cout instead of std::cout. We will explai it more 
+in detail later in 1.5.3 Namespace.
+*/
+using namespace std;
+
+//create the main function, which is a required function in any C++ program
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "rosbot_node");
+
+  RosbotClass rosbot;
+  rosbot.move();
+  /*
+  In this particular case, passing number 1 to the method means 
+  we are asking for the coordinate x, number 2 for coordinate y and 
+  number 3 for coordinate z.
+  C++ is very demanding, it needs us to specify every type of 
+  variable we are using, even before using it. 
+  */
+  float coordinate = rosbot.get_position(1);
+
+  ROS_INFO_STREAM(coordinate);
+  
+  /*
+  return 0; is used to indicate 
+  the successful completion of the program.
+  Remember that the main function of a program is defined 
+  as int main(), which means that it is expected to 
+  return an integer value. The integer value returned by the 
+  main function is called the exit status or return code of 
+  the program. return 0; is for success and return 1; or 
+  other numbers for failures.
+  */
+  return 0;
